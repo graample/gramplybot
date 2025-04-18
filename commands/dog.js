3c47ff6a16f2f67f325eaa7f3e94f0dbcd201bdc
Raymond Yee <haesik.yee@gmail.com>
2025-04-18 09:53:37 -0700

    implement dog command

---
+++ commands/dog.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { MessageFlags, SlashCommandBuilder } from 'discord.js';
+
+const create = () => {
+  const command = new SlashCommandBuilder()
+    .setName('dog')
+    .setDescription('finds a random dog image');
+
+  return command.toJSON();
+};
+
+const invoke = async (interaction) => {
+  axios('https://dog.ceo/api/breeds/image/random')
+  .then(imgResult => {
+    interaction.reply({
+      content: imgResult.data.message,
+    })
+  })
+  .catch((error) => {
+    console.log('Error replying to Dog');
+    console.error(error);
+  });
+};
+
+export { create, invoke };
